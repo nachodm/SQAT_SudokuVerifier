64826a402979043c59d92914c403712d53ed9f51
idomingo <idomingo@10.30.4.11>
2016-09-07 16:59:48 +0300

    -1 error

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -2,7 +2,17 @@
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
+		
+		for(int i = 0; i < candidateSolution.length(); i++) {
+			char [] sudoku = candidateSolution.toCharArray();
+			if (sudoku[i] < 0) {
+				return -1;
+			}
+		}
 		// returns 0 if the candidate solution is correct
+		
+		
+		
 		return 0;
 		
 		//check
